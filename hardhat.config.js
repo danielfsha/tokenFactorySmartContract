b6b7d264e5580d9bc8bc2322535157c2300e70db
daniel fisseha <developedbydanielfisseha@gmail.com>
2024-06-23 20:15:15 +0300

    changed the EVM chain to polygon ZkEVM Cardona

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,8 +21,8 @@ const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 module.exports = {
   solidity: "0.8.24",
   networks: {
-    sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+    polygonZkEVMCardona: {
+      url: `https://polygonzkevm-cardona.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY]
     }
   },
